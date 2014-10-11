445e1039b9087073e5924691ae1c3f37db8a54fd
Vibin K <vibinkrishnadas42@gmail.com>
2014-10-12 03:32:27 +0530

    Donation Pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var appName="";
 var data;
+var currentMainPage;
 function setMainVars(){
 //jsoncall for main variables
 appName="Maatran";
@@ -8,22 +9,18 @@ $("#appName").html(appName);
 
 
 function renderDonationPage(){
-	
-
-}
-
-function renderEventPage(){
+	$("#subMenu").html('');
 	$.ajax({
-		url:'http://192.168.115.73:8080/NGO/api/donations/student',
-		method:'GET',
-		type:'json',
-		async:false,
-		success: function(jsonData){
-			console.log(jsonData);
-			data=jsonData;
-		}
-	});
-	var type=[];
+			url:'http://192.168.115.73:8080/NGO/api/donations/student',
+			method:'GET',
+			type:'json',
+			async:false,
+			success: function(jsonData){
+				console.log(jsonData);
+				data=jsonData;
+			}
+		});
+  	var type=[];
   	var count=0;
   	var flag=0;
 	$(data).each(function(i,val){
@@ -40,15 +37,24 @@ function renderEventPage(){
     	}
     	flag=0;
 		});
+
+	$("#subMenu").css("padding-top","20px").append("<div class='submenu' id='blood' >Donate Blood</div>");
+
+}
+
+function renderEventPage(){
+
+
 }
 
 
 
 $(document).on("click",".page-scroll",function(){
 	var base=this;
-	if($(this).children("a").text()==="Donations"){
+
+	if($(this).children("a").text()==="Donations" && $(this).children("a").text() != currentMainPage){
 		renderDonationPage();
-	}else if($(this).children("a").text()==="Events"){
+	}else if($(this).children("a").text()==="Events" && $(this).children("a").text() != currentMainPage){
 		renderEventPage();
 	}
 });
@@ -70,7 +76,7 @@ $(document).on("click",".submenu",function(){
 			pic=$('<img>').addClass(val.type+"pic").attr('src','images/userEmpty.gif');
 			container2=$('<div>').addClass(val.type+"container2");
 			container3=$('<div>').addClass(val.type+"container3");
-			title=$('<div>').addClass(val.type+"title").text(val.name).css('font-weight','bold');
+			title=$('<div>').addClass(val.type+"title").text(val.title).css('font-weight','bold');
 			description=$('<div>').addClass(val.type+"description").text(val.description+" Education:"+val.education);
 			button=$('<button>').addClass(val.type+"button btn btn-primary").text("Donate").css('background-color','#428bca').css('border-color','#357ebd');
 			button2=$('<button>').addClass(val.type+"button btn btn-primary").text("Share").css('background-color','#5cb85c').css('border-color','#5cb85c');
@@ -88,5 +94,16 @@ $(document).on("click",".submenu",function(){
 		}
     });
 	
+});
+
+
+$(document).on("click","#blood",function(){
+	$("#subBody").html('');
+	$("#subBody").css('margin-top','20px').append("<div class='row' style='margin-bottom:10px'><div class='col-md-3' style='text-align:right;padding-top:10px;'><label>Name</label></div><div class='col-md-1'></div><div class='col-md-7'><input class='form-control' placeholder='Username' name='username'></div></div>");
+	$("#subBody").css('margin-top','20px').append("<div class='row' style='margin-bottom:10px'><div class='col-md-3' style='text-align:right;padding-top:10px;'><label>Email</label></div><div class='col-md-1'></div><div class='col-md-7'><input class='form-control' placeholder='Email' name='email'></div></div>");
+	$("#subBody").css('margin-top','20px').append("<div class='row' style='margin-bottom:10px'><div class='col-md-3' style='text-align:right;padding-top:10px;'><label>Phone Number</label></div><div class='col-md-1'></div><div class='col-md-7'><input class='form-control' placeholder='Phone Number' name='phone'></div></div>");
+	$("#subBody").css('margin-top','20px').append("<div class='row' style='margin-bottom:10px'><div class='col-md-3' style='text-align:right;padding-top:10px;'><label>Blood Group</label></div><div class='col-md-1'></div><div class='col-md-7'><input class='form-control' placeholder='Blood Group' name='blood'></div></div>");
+	$("#subBody").css('margin-top','20px').append("<div class='row' style='margin-bottom:10px'><div class='col-md-3' style='text-align:right;padding-top:10px;'><label>Address</label></div><div class='col-md-1'></div><div class='col-md-7'><input class='form-control' placeholder='Address' name='address'></div></div>");
+	$("#subBody").css('margin-top','20px').append("<div class='row' style='margin-bottom:10px'><div class='col-md-3' style='text-align:right;padding-top:10px;'><label>Available Time</label></div><div class='col-md-1'></div><div class='col-md-7'><input class='form-control' placeholder='Available Time' name='time'></div></div>");
 });
 
